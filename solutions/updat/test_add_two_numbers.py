289c6e721ffcb3c4153fcd8e9d9dc9d53bbf2dc2
manezhah <mohmandmanezha@gmail.com>
2025-01-04 21:45:04 -0500

    add

---
+++ solutions/updat/test_add_two_numbers.py
@@ -0,0 +1,20 @@
+'''created 2025 -01-04
+
+@author''Manezhah mohmand
+
+
+
+import unittest
+form solutions.add- two-numbers import add-numbcrs
+
+class TestAddTwoNumbers(unittest. TestCase):
+"""The test for testing add numbers function.add"""
+
+ def test_positive_numbers(setf):
+    """It shoudld be positive numbers"""'''
+    self.ossertEqual(add_numbers(2,3),5)
+    
+    
+    
+    
+    
